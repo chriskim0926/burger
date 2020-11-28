b9deba7cc7f8d748213039ec812060b3ee83d89f
chris kim <chriskim0926@gmail.com>
2020-11-28 16:42:59 -0500

    database created with schema and seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
 INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Vegi Burger');
+
+
